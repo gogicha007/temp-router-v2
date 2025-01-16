ef6c07e61143b0c889d5dfc2f7daf01a4ae4a879
gogicha007 <gogicha@gmail.com>
2025-01-16 09:56:46 +0400

    chore: save the progress

--- src/components/bills.jsx
+++ src/components/bills.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import { getInvoice } from './data';
+
 
 const Bills = () => {
-    let params = useParams()
-    return (
-    <div>Bills</div>
-  )
-}
+  let params = useParams();
+  console.log(parseInt(params.invoiceId))
+  let invoice = getInvoice(parseInt(params.invoiceId));
 
-export default Bills
+  return (
+    <main className="flex flex-col py-5 border my-4 w-1/3 mx-auto">
+      <h2>Total Due: {invoice.amount}</h2>
+      <p>{invoice.name}:{invoice.number}</p>
+      <p>Due Date: {invoice.due}</p>
+    </main>
+  );
+};
+
+export default Bills;
--- src/components/data.js
+++ src/components/data.js
@@ -31,6 +31,10 @@ let invoices = [
   },
 ];
 
-export function getInvoices(){
-    return invoices;
+export function getInvoices() {
+  return invoices;
+}
+
+export function getInvoice(number) {
+  return invoices.find((invoice => invoice.number === number));
 }
